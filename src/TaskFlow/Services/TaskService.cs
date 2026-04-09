57dc1b4c54f4e5a5bd7fbcaac07c5074b88b637f
Ferdiaz99 <diazfernando209@gmail.com>
2026-04-09 18:06:52 -0300

    feat: agregar método ListarTareas en TaskService

--- src/TaskFlow/Services/TaskService.cs
+++ src/TaskFlow/Services/TaskService.cs
@@ -1,16 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TaskFlow.Models;
+﻿using TaskFlow.Models;
 using TaskFlow.Utils;
 
 namespace TaskFlow.Service.cs
 {
-    
     public class TaskService
     {
-        public CrearTarea() { }
+        public void CrearTarea() { }
+
+        public void ListarTareas()
+        {
+            List<TaskItem> tareas = FileManager.cargar();
+
+            if (tareas.Count == 0)
+            {
+                Console.WriteLine("No hay tareas registradas.");
+                return;
+            }
+
+            Console.WriteLine($"\n===== LISTA DE TAREAS ({tareas.Count}) =====\n");
+            foreach (TaskItem tarea in tareas)
+            {
+                Console.WriteLine(tarea.ToString());
+            }
+        }
     }
 }
