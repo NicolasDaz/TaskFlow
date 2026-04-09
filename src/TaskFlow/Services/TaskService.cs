702a168a20fb36856fe63772a77381be023c4133
unknown <nicolasdaza20232030@gmail.com>
2026-04-09 18:24:22 -0300

    Implementacion de nuevo la logica para guardar nuevas tareas

--- src/TaskFlow/Services/TaskService.cs
+++ src/TaskFlow/Services/TaskService.cs
@@ -11,5 +11,33 @@ namespace TaskFlow.Service.cs
     
     public class TaskService
     {
+
+        public List<TaskItem> Tareas = FileManager.cargar();
+        public void CrearTarea()
+        {
+            TaskItem tarea = new TaskItem();
+            try
+            {
+                Console.WriteLine("Ingrese el título de la tarea:");
+                tarea.Title = Console.ReadLine();
+                Console.WriteLine("Ingrese la descripción de la tarea (opcional):");
+                tarea.Description = Console.ReadLine();
+                Console.WriteLine("Ingrese el responsable de la tarea:");
+                tarea.Responsible = Console.ReadLine();
+                tarea.Id = GenerarId();
+                Tareas.Add(tarea);
+                Console.WriteLine("Tarea creada exitosamente.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error se ha encontrado un error {ex.Message}");
+            }
+        }
+
+        public int GenerarId()
+        {
+            if (Tareas.Count == 0) return 1;
+            return Tareas.Max(t => t.Id) + 1;
+        }
     }
 }
