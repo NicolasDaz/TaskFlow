b6f1e58f74df8264f6c899e1f6ed4f48a09b60c6
romina sofia dominguez <rdominguez@mi.unc.edu.ar>
2026-04-14 13:47:14 -0300

    Reajuste final de mensaje

--- src/TaskFlow/Services/TaskService.cs
+++ src/TaskFlow/Services/TaskService.cs
@@ -6,11 +6,28 @@ using System.Threading.Tasks;
 using TaskFlow.Models;
 using TaskFlow.Utils;
 
-namespace TaskFlow.Service.cs
+namespace TaskFlow.Service
 {
     
     public class TaskService
     {
-        public CrearTarea() { }
+        private List<TaskItem> tasks = new List<TaskItem>();
+
+        public void UpdateTaskStatus(int id, Status newStatus)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+
+
+            if (task != null)
+            {
+                task.Estado = newStatus;
+                task.UpdateAt = DateTime.Now;
+                Console.WriteLine("Estado actualizado correctamente!");
+            }
+            else
+            {
+                Console.WriteLine("No se encontro la tarea");
+            }
+        }
     }
 }
